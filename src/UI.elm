7aad83138c3d4a9a71185fcff84e87e495a1a6fe
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-05-16 15:46:04 -0300

    Initial commit

---
+++ src/UI.elm
@@ -0,0 +1,171 @@
+module UI exposing (PageModel, layout, pageConfig)
+
+import Array
+import Gen.Route as Route exposing (Route)
+import Html exposing (Attribute, Html, a, div, header, main_, nav, text)
+import Html.Attributes exposing (class, classList, href, id, tabindex)
+import Regex
+
+
+
+-- Model
+
+
+type alias PageModel msg =
+    { route : Route
+    , mainContent : List (Html msg)
+    , mainAttrs : List (Attribute msg)
+    }
+
+
+type alias Link =
+    { routeStatic : Route
+    , routeReceived : Route
+    , routeName : String
+    , hasMarginLeft : Bool
+    }
+
+
+pageConfig : PageModel msg
+pageConfig =
+    { route = Route.Home_
+    , mainContent = []
+    , mainAttrs = []
+    }
+
+
+defaultLink : Link
+defaultLink =
+    { routeStatic = Route.Home_
+    , routeReceived = Route.Home_
+    , routeName = ""
+    , hasMarginLeft = False
+    }
+
+
+
+-- Structure
+
+
+isRoute : Route -> Route -> Bool
+isRoute route compare =
+    case ( route, compare ) of
+        ( Route.Home_, Route.Home_ ) ->
+            True
+
+        _ ->
+            False
+
+
+caseNamePage : Route -> String
+caseNamePage route =
+    case route of
+        Route.Home_ ->
+            "Home"
+
+        Route.About ->
+            "About"
+
+        Route.NotFound ->
+            "Not Found"
+
+
+userReplace : String -> (Regex.Match -> String) -> String -> String
+userReplace userRegex replacer string =
+    case Regex.fromString userRegex of
+        Nothing ->
+            string
+
+        Just regex ->
+            Regex.replace regex replacer string
+
+
+classBuilder : String -> String
+classBuilder string =
+    userReplace "[ ]" (\_ -> "-") string
+        |> String.toLower
+
+
+
+-- View
+
+
+layout : PageModel msg -> List (Html msg)
+layout model =
+    let
+        mainClass : Attribute msg
+        mainClass =
+            class <| "main--" ++ classBuilder (caseNamePage model.route)
+    in
+    [ div
+        [ id "root"
+        , placeholderStyles 0
+        , classList [ ( "scroll", True ), ( "root--" ++ classBuilder (caseNamePage model.route), True ) ]
+        ]
+        [ viewHeader model
+        , main_ (mainClass :: model.mainAttrs) model.mainContent
+        ]
+    ]
+
+
+viewHeader : PageModel msg -> Html msg
+viewHeader model =
+    header [ class "main-header" ]
+        [ viewHeaderLinks model [ Route.Home_, Route.About ]
+            |> nav
+                [ class "main-header__nav"
+                , placeholderStyles 1
+                ]
+        ]
+
+
+viewHeaderLinks : PageModel msg -> List Route -> List (Html msg)
+viewHeaderLinks model links =
+    List.map
+        (\staticRoute ->
+            viewLink
+                { defaultLink
+                    | routeName = caseNamePage staticRoute
+                    , routeStatic = staticRoute
+                    , routeReceived = model.route
+                }
+        )
+        links
+
+
+viewLink : Link -> Html msg
+viewLink model =
+    a
+        [ class "main-header__links"
+        , placeholderStyles 2
+        , classList
+            [ ( "main-header__links--current-page"
+              , isRoute model.routeReceived model.routeStatic
+              )
+            , ( "main-header__links--margin-left", model.hasMarginLeft )
+            ]
+        , href <| Route.toHref model.routeStatic
+        , tabindex 1
+        ]
+        [ text model.routeName ]
+
+
+placeholderStyles : Int -> Attribute msg
+placeholderStyles index =
+    let
+        listOfStyles : List (Attribute msg)
+        listOfStyles =
+            [ class "grid grid-rows-[min-content,auto] gap-8"
+            , class "flex justify-center gap-4 text-2xl bg-surface-1 shadow-inner"
+            , class "p-4 font-semibold md:p-8"
+            ]
+
+        arrayOfStyles : Array.Array (Attribute msg)
+        arrayOfStyles =
+            Array.fromList listOfStyles
+
+        getStyle : Maybe (Attribute msg)
+        getStyle =
+            Array.get index arrayOfStyles
+    in
+    Maybe.withDefault (class "error") getStyle
